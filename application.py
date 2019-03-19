12bf8e08cbc4f3d240f111c74ffed7abded76a3e
Austin <ajhartshorn@wpi.edu>
2019-03-19 19:37:33 -0400

    Broadcast and receive works... just need to test randomizing

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import serial
 import RPi.GPIO as GPIO
 import threading
@@ -6,7 +8,7 @@ import time
 import argparse
 
 # setup the GPIO pins / transceiver
-IP = "70.150.145.199"
+IP = "1.2.3.4"
 teamMACS = ["5B-66-5A-D5-92-CC", "80-AB-14-FA-9F-A7"]
 teamPASS = ["Password321", "Password123"]
 
@@ -80,7 +82,7 @@ def main():
     v.join()
 
     print("Finished Init\n\r")
-    while(1)
+    while(1):
         time.sleep(.5)
 
 
@@ -88,7 +90,7 @@ def main():
 # example packet: 70.150.145.199|80-AB-14-FA-9F-A7|Password123|Hello
 def receiver():
     print("Running receiver thread\n")
-    while(0):
+    while(1):
         time.sleep(2)
         radio.acquire()
         data = ser.readline()
@@ -96,20 +98,26 @@ def receiver():
         data = data.decode().split("|")
         
         # check that the IP of this packet belongs to this node
-        if(data[0] != IP):
+        if(data[0] == IP):
+            print("This packet belongs to this node\n")
+        elif(data[0] != IP):
             print("This packet belongs to IP: " + data[0] + "\n\r")
             continue
-    
-        print("This packet belongs to this node\n")
-        
-        # check that the MAC address is in the list of team MACs
-        if(data[1] not in teamMACS):
-            print("This is not a valid MAC address\n\r")
-            radio.acquire()
-            ser.write("INV_MAC:" + data[1] + "\n")
-            radio.release()
+        else:
             continue
         
+        # check that the MAC address is in the list of team MACs
+        try:
+            if(data[1] not in teamMACS):
+                print("This is not a valid MAC address\n\r")
+                radio.acquire()
+                ser.write("INV_MAC:" + data[1] + "\n")
+                radio.release()
+                continue
+        except IndexError:
+            print("Incomplete packet structure detected")
+            continue
+
         # check if the device has been authenticated before
         if(data[1] not in authDevices):
             print("Authenticating Device\n")
@@ -134,7 +142,7 @@ def broadcastIP():
     while(1):
         print("Broadcasting")
         radio.acquire()
-        ser.write(IP)
+        ser.write(IP+'\n')
         radio.release()
         print("Broadcasted my IP\r\n")
         time.sleep(2)
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
 import time
 import serial
-import requests
+import threading
+import argparse
+import RPi.GPIO as GPIO
 
 ser = serial.Serial(
 
@@ -15,20 +16,50 @@ ser = serial.Serial(
     )
 
 counter=0
-stringtest = "Hello from raspi"
+IP = "192.215.16.754\n"
 
-def broadcast():
-    ser.write(stringtest)
-    time.sleep(1)
-
+radio = threading.Lock()
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help="the IP address of this node", default="")
+    parser.add_argument("-b", "--baud", help="initial baud rate of the node, likely to change with the randomizer", default=9600, type=int)
+    args = parser.parse_args()
+    
+    IP = args.ip
+    baud = args.baud
+    
+    GPIO.setmode(GPIO.BOARD)
+    
+    t = threading.Thread(target=receive)
+    u = threading.Thread(target=broadcast)
+    t.start()
+    u.start()
+    t.join()
+    u.join()
     while 1:
+        time.sleep(.5)
+    #while 1:
     #x=ser.readline()
     #c=x.decode().split("|")
     #if len(c) > 1:
     #    print(c)
-        broadcast()
+    #    broadcast()
+
+
+def broadcast():
+    while(1):
+        radio.acquire()
+        print("broadcasting")
+        ser.write(IP)
+        radio.release()
+        time.sleep(1)
+
+
+def receive():
+    print("Receiving")
+    time.sleep(2)
+
 
 if __name__ == "__main__":
     main()
